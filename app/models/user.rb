39977bb9ddd71465b85832fc3d267733d3948280
HamaguchiRyosuke <soccer1356abc@gmail.com>
2022-01-29 23:37:01 +0900

    Implement hundling log_in by current_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost? ? Bcrypt::Engine::MIN_COST : Bcrypt::Engine.cost
+    Bcrypt::Password.create(string, cost: cost)
+  end
 end
