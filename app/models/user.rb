bd696660254d6c4cedd99114061804d59c9aadb3
HamaguchiRyosuke <soccer1356abc@gmail.com>
2022-02-17 22:26:34 +0900

    Add validates and relations for the micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   before_save :downcase_email
   before_create :create_activation_digest
 
+  has_many :microposts
+
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
