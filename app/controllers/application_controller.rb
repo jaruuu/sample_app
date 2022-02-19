15a9bab5a9e72c2a2e99cbb2b7762f4a37decf38
HamaguchiRyosuke <soccer1356abc@gmail.com>
2022-02-19 18:53:23 +0900

    Implment destroying micropost in home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
-
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
