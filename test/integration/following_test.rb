e112f55275f8c73a7a1eeb478ee4c3d48f412d49
HamaguchiRyosuke <soccer1356abc@gmail.com>
2022-03-01 14:21:43 +0900

    Implement working follow and unfollow button by ajax

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class FollowingTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
     log_in_as(@user)
   end
 
@@ -21,6 +22,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "should follow a user by the standard way" do
+    assert_difference "Relationship.count", 1 do
+      post relationships_path, params: { followed_id: @other_user.id }
+    end
+  end
+
+  test "should follow a user by the ajax" do
+    assert_difference "Relationship.count", 1 do
+      post relationships_path, params: { followed_id: @other_user.id }, xhr: true
+    end
+  end
+
+  test "should unfollow a user by the standard way" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference "Relationship.count", -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user by the ajax" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference "Relationship.count", -1 do
+      delete relationship_path(relationship), xhr: true
     end
   end
 end
