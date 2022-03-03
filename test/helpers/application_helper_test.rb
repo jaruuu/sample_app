369aee3396fb7cded72f0520a3dfa42e7fff3d38
HamaguchiRyosuke <soccer1356abc@gmail.com>
2022-03-03 15:30:21 +0900

    Refactoring a test for ApplicationHelper#full_title

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    page_title = "Help"
+
+    assert_equal full_title, base_title
+    assert_equal full_title(page_title), "#{page_title} | #{base_title}"
   end
 end
